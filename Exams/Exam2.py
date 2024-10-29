001b3148be629ab21b748acf4d04e3c55c6ef2f9
Fernando Iquiza <fmiquiza@gmail.com>
2024-10-29 05:49:14 -0400

    Identificando super clase o clase padre (Herencia)

---
+++ Exams/Exam2.py
@@ -0,0 +1,27 @@
+class Clase:
+    def __init__(self) -> None:
+        self.marca = marca
+        self.modelo = modelo
+        
+    def get_marca(self) -> str: 
+        return self.marca
+    def get_tipo(self) -> str: 
+        return self.modelo
+    def encender(self) -> None: 
+        print("Encendiendo vehículo")
+    def detener(self) -> None:
+        print("Deteniendo vehículo")  
+        
+class Clase:
+    def __init__(self) -> None:
+        super().__init__(marca, modelo)
+        self.puertas = puertas
+    def get_puertas(self) -> int: 
+        return self.puertas    
+       
+    
+class Clase:
+    def __init__(self) -> None:
+        super().__init__(marca, modelo) 
+        self.amortiguacion = amortiguacion
+        
