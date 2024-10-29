5f36f56a25677f98afbb130c498eccf3814c0a4e
Fernando Iquiza <fmiquiza@gmail.com>
2024-10-29 06:20:36 -0400

    Agregamos logica para validar que el anho de fabricacion del coche este dentro de los anhos permitidos para circular, instanciamos la clase, y llamamos solo a ese metodo, verificamos que cumpla la logica y funcione, por el momento esta hardcodeado

--- Exams/Exam2.py
+++ Exams/Exam2.py
@@ -17,9 +17,19 @@ class Vehiculo:
 # - Si el valor ingresado de las puertas está entre 2-5, continuar con la validación del año, si no, mostrar un mensaje de error como “Revise nuevamente el número de puertas ingresado ya que un automovil/coche no existe uno que tenga [puertas] (valor de puertas, ejemplo: 9)
 # - Validar que el año proporcionado está presente en el arreglo, se debe mostrar el mensaje: "El coche con año de fabricación [año] tiene permitido circular." (Donde "[año]" es el valor introducido por el usuario). Si el año NO está en el arreglo, se debe mostrar: "El coche con año de fabricación [año] NO puede circular en Bolivia.”
 class Coche(Vehiculo):
-    def __init__(self,marca, modelo, puertas) -> None:
+    def __init__(self,marca, modelo, puertas, añoFabricacionCoche) -> None:
         super().__init__(marca, modelo)
         self.puertas = puertas
+        
+        self.añoFabricacionCoche = añoFabricacionCoche
+        self.añosValidos = [2020,2021,2022,2023,2024]  # Años de fabricación válidos
+    
+    def validarAnhoDeCirculacion(self):
+        if self.añoFabricacionCoche in self.añosValidos:
+            print(f"Este coche fabricado en {self.añoFabricacionCoche} tiene permitida la circulación")
+        else:
+            print(f"Este coche fabricado en {self.añoFabricacionCoche} ya no puede circular en territorio Boliviano")
+        
     def get_puertas(self) -> int: 
         return self.puertas    
     def encender(self) -> None: 
@@ -32,15 +42,14 @@ class Coche(Vehiculo):
 # - Si la amortiguación es "Simple" o "Doble", se debe mostrar: "La bicicleta tiene una amortiguación [tipo]." (Donde "[tipo]" es el valor ingresado por el usuario).
 # - Si no es ninguno de estos valores, se debe mostrar: "No existe una amortiguación [tipo].”
 class Bicicleta(Vehiculo):
-    def __init__(self, marca, modelo, amortiguacion, añoFabricacionCoche) -> None:
+    def __init__(self, marca, modelo, amortiguacion) -> None:
         super().__init__(marca, modelo) 
         self.amortiguacion = amortiguacion
-        
-        self.añoFabricacionCoche = añoFabricacionCoche
-        self.añosValidos = [2020,2021,2022,2023,2024]  # Años de fabricación válidos
-    
         
     def encender(self) -> None: 
         print("Encendiendo Bicicleta")
     def detener(self) -> None:
         print("Deteniendo Bicicleta")  
+        
+auto = Coche("Nissan", "Murano", 4, 2023)
+auto.validarAnhoDeCirculacion()
