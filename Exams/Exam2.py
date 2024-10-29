906965e6ca655706ab96fe0e371d68d6e422abb1
Fernando Iquiza <fmiquiza@gmail.com>
2024-10-29 06:07:33 -0400

    Dado que nos habla un ejercicio de un Coche, y esta clase base ya tiene un parametro -puertas-, entonces ya tenemos la referencia de que este enunciado tiene relacion con esta base, y refactorizamos cambiando el nombre de clase a Coche para mayor legibilidad

--- Exams/Exam2.py
+++ Exams/Exam2.py
@@ -12,17 +12,20 @@ class Vehiculo:
     def detener(self) -> None:
         print("Deteniendo vehículo")  
         
-class Clase1(Vehiculo):
+# REFACTORIZAR Una clase Hija o SubClase, debe implementar una nueva función que utilice un nuevo arreglo/lista con valores (2020, 2021, 2022, 2023, 2024) para los distintos años Permitidos De Circulación.
+# - Esta función debe recibir un parámetro correspondiente al año de fabricación de un coche, así como crear el arreglo mencionado arriba.
+# - Si el valor ingresado de las puertas está entre 2-5, continuar con la validación del año, si no, mostrar un mensaje de error como “Revise nuevamente el número de puertas ingresado ya que un automovil/coche no existe uno que tenga [puertas] (valor de puertas, ejemplo: 9)
+# - Validar que el año proporcionado está presente en el arreglo, se debe mostrar el mensaje: "El coche con año de fabricación [año] tiene permitido circular." (Donde "[año]" es el valor introducido por el usuario). Si el año NO está en el arreglo, se debe mostrar: "El coche con año de fabricación [año] NO puede circular en Bolivia.”
+class Coche(Vehiculo):
     def __init__(self,marca, modelo, puertas) -> None:
         super().__init__(marca, modelo)
         self.puertas = puertas
     def get_puertas(self) -> int: 
         return self.puertas    
-    
     def encender(self) -> None: 
-        print("Encendiendo clase1")
+        print("Encendiendo Coche")
     def detener(self) -> None:
-        print("Deteniendo clase1")  
+        print("Deteniendo Coche")  
        
     
 class Clase2(Vehiculo):
