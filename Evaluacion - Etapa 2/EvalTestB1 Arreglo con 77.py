00cf1997529bad5876960c238d7927c71bd7449b
Fernando Iquiza <fmiquiza@gmail.com>
2024-11-06 08:35:17 -0400

    Solucion Python - Evaluacion 5 puntos 2da etapa

---
+++ Evaluacion - Etapa 2/EvalTestB1 Arreglo con 77.py
@@ -0,0 +1,21 @@
+numeros = [23, 44, 67, 12, 88, 34, 56, 77, 90, 11, 23]
+resultado = ""
+cont_par = 0
+cont_impar = 0
+
+for i in range(len(numeros)):
+    num = numeros[i]
+    if num % 2 == 0:
+        resultado += "par"
+        cont_par += 1
+    else:
+        resultado += "impar"
+        cont_impar += 1
+    
+    if i < len(numeros) - 1:  
+        resultado += ", "
+
+print(f"Resultados: {resultado}")
+print(f"Cantidad de pares: {cont_par}")
+print(f"Cantidad de impares: {cont_impar}")
+
